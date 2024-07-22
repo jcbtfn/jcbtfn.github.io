b9db2f49709c2f693cd995489a7a00829a2b55a4
Jacob Miranda <jacob.tfn@gmail.com>
2024-07-22 13:45:01 +0100

    web rework

--- js/script.js
+++
@@ -1,7 +0,0 @@
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text).then(function() {
-        //alert('Texto copiado al portapapeles: ' + text);
-    }, function(err) {
-        console.error('Error al copiar al portapapeles: ', err);
-    });
-}
