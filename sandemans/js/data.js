d421bdf7909318e2af6d66484efb225cbf8bd46a
Jacob Miranda <jacob.tfn@gmail.com>
2025-12-09 14:55:08 +0000

    json mod sandemans

---
+++ sandemans/js/data.js
@@ -0,0 +1,27 @@
+{
+    "centro": [
+      {
+        "name": "Mercato Mayfair",
+        "img": "https://lh3.googleusercontent.com/p/AF1QipMbE4DXZ40XC2g3koLsKJB2jhbTjgp3evDQLUsZ=s1360-w1360-h1020",
+        "url": "https://g.co/kgs/zr8cZ3Y",
+        "description": "Mercato Mayfair, una iglesia desconsagrada..."
+      },
+      {
+        "name": "El Pollote",
+        "img": "https://lh3.googleusercontent.com/p/AF1QipMkF6ITVb2wSz_zN7KvMPaIX3NrWv3lKHNpU1Ln=s1360-w1360-h1020",
+        "url": "https://g.co/kgs/u5E7DPG",
+        "description": "Es un restaurante sin gluten..."
+      }
+    ],
+  
+    "desayuno": [
+      {
+        "name": "Regency Cafe",
+        "img": "https://lh3.googleusercontent.com/p/AF1QipOdy11-pdwm0vCToEOsg4izjD6GZCUAQNlVmtyw=s1360-w1360-h1020",
+        "url": "https://g.co/kgs/QmutfX4",
+        "description": "Una de las cafeterías de época..."
+      }
+    ]
+  
+  }
+  
